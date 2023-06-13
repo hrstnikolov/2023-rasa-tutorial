003ef88a025ef2b882876776734933ddaa37240f
Hristo <hrstnikolov@gmail.com>
2023-06-13 09:29:29 +0300

    updates

--- dentist_02/actions/actions.py
+++ dentist_02/actions/actions.py
@@ -1,27 +1,40 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from enum import Enum, auto
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+# See https://docs.python.org/3/howto/enum.html#using-automatic-values
+class AutoName(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name.lower()
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+class Weekday(Enum):
+    Monday = auto()
+    Tuesday = auto()
+    Wednesday = auto()
+    Thursday = auto()
+    Friday = auto()
+    Saturday = auto()
+    Sunday = auto()
+
+
+class ActionCheckDateAvailability(Action):
+    def name(self) -> Text:
+        return "check_date_availability"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        weekday = tracker.get_slot("weekday")
+        if weekday in [Weekday.Saturday, Weekday.Sunday]:
+            dispatcher.utter_message(response="utter_fully_booked")
+        else:
+            dispatcher.utter_message(response="utter_confirm_availability")
+
+        return []
